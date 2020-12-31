8682d2b4cfd763517e14cb875428de48d7233fca
akunitski <akunitski@allot.com>
2020-12-31 13:58:41 +0300

    Big commit 3

---
+++ src/main/java/TheOffice.java
@@ -0,0 +1,24 @@
+public class TheOffice {
+    public static String outed(Person[] meet, String boss) {
+
+        int counter = 0;
+
+        for (Person per : meet) {
+
+            if (per.name.equals(boss)) {
+                counter += per.happiness * 2;
+            } else
+
+                counter += per.happiness;
+        }
+        System.out.println((double) counter/meet.length);
+        if(counter/meet.length<=5){
+            return "Get Out Now!";
+        } else
+            return "Nice Work Champ!";
+
+
+
+
+    }
+}
