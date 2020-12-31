4ca2f9fa245f80eb83585c39a47599f5d4075746
akunitski <akunitski@allot.com>
2020-12-31 11:58:56 +0300

    Big commit 2

---
+++ src/test/java/Kata2Test.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+
+
+public class Kata2Test {
+
+
+    public void TestSomething(){
+        assertArrayEquals(new int[]{}, Kata2.countPositivesSumNegatives(null));
+    }
+
+}
